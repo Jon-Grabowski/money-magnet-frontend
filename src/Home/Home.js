624da531ec1f96d168dace1a7b1307ada9e19f22
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-12 14:53:31 -0400

    basic login form setup

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import Login from '../Signup-Login/Login'
 
 function Home() {
     return (
-        <div className='flex justify-center items-center container mx-auto border'>
+        <div className='flex flex-wrap justify-center items-center container mx-auto border gap-x-1 md:gap-x-10'>
             <div>
                 <h1 className=''>LOGO GOES HERE</h1>
             </div>
--- src/Signup-Login/Login.js
+++ src/Signup-Login/Login.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useFormik } from 'formik';
 
 function Login() {
+
+    const initialValues = {
+        email:'',
+        password:''
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit: (values)=>{console.log(values)}
+    });
+
     return (
         <div>
-            <h1>Log in here</h1>
+            <form onSubmit={formik.handleSubmit} className='flex flex-col gap-y-1'>
+                <input 
+                    type='text' 
+                    value={formik.values.email}
+                    name='email'
+                    onChange={formik.handleChange}
+                    placeholder='email...'
+                    className=''
+                    required
+                ></input>
+                <input 
+                    type='password' 
+                    value={formik.values.password}
+                    name='password'
+                    onChange={formik.handleChange}
+                    placeholder='password...'
+                    className=''
+                    required
+                ></input>
+                <button 
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' 
+                    type='submit'
+                >
+                    Log In
+                </button>
+            </form>
         </div>
     )
 }
