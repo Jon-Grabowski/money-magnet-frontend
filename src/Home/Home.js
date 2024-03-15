80d63bc10b894e552a0c7c4563ce499190dd7708
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-15 15:50:12 -0400

    setting up top section of homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Logo from '../Logo'
-import HomeNav from './HomeNav'
+import './home.css'
+import { Link } from 'react-router-dom'
+import { firstParagraph } from './utilitiesHome'
 
 function Home() {
     return (
-        <div>
-            <HomeNav />
-            <div style={{marginTop:'2.5rem'}} className='m-auto border w-full'>
+        <div className='mt-10 container'>
+            <div className='mt-20 flex flex-wrap justify-around'>
+                <div className='md:w-1/2 flex items-center'>
+                    <p className='font-bold text-start'>
+                        {firstParagraph}
+                    </p>
+                </div>
+                <div className='md:w-1/4 flex flex-col gap-4 justify-center'>
+                    <h1>Start your path to financial freedom today!</h1>
+                    <Link to='/signup'><button>Sign Up</button></Link>
+                    <p>Already have an account? <Link to='/login'>Log In</Link></p>
+                </div>
+            </div>
+            {/* <div style={{marginTop:'2.5rem'}} className='w-full border'>
                 <div className='flex flex-wrap justify-center mt-5'>
                     <div className='max-w-80'>
                         <p>
@@ -17,7 +30,7 @@ function Home() {
 
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
         
     )
